a7efe55f59609fdf82ed530dc25edf25ac9956c8
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-07-11 17:23:47 +0530

    Fixed bug for Calculation

--- EXPENSE-TRACKER/JS/calculation.js
+++ EXPENSE-TRACKER/JS/calculation.js
@@ -2,31 +2,36 @@ const totalIncome = document.getElementById("totalIncome");
 const totalExpense = document.getElementById("totalExpense");
 const totalBalance = document.getElementById("totalBalance");
 
-let calTotalIncome = parseInt(0);
-let calTotalExpense = parseInt(0);
-let calTotalBalance = parseInt(0);
+let calTotalIncome = 0;
+let calTotalExpense = 0;
+let calTotalBalance = 0;
 
 function calculateIncome() {
+  calTotalIncome = 0;
   transactions.forEach((transaction) => {
     if (transaction.type === "Income") {
       calTotalIncome = calTotalIncome + parseInt(transaction.amount);
-      totalIncome.textContent = calTotalIncome;
+      totalIncome.textContent = `₹${calTotalIncome.toLocaleString("en-IN")}`;
     }
   });
 }
 
 function calculateExpense() {
+  calTotalExpense = 0;
   transactions.forEach((transaction) => {
     if (transaction.type === "Expense") {
       calTotalExpense = calTotalExpense + parseInt(transaction.amount);
-      totalExpense.textContent = calTotalExpense;
+      totalExpense.textContent = `₹${calTotalExpense.toLocaleString("en-IN")}`;
     }
   });
 }
 
 function calculateBalance() {
-  transactions.forEach((transaction) => {
-    calTotalBalance = calTotalIncome - calTotalExpense;
-    totalBalance.textContent = calTotalBalance;
-  });
+  calTotalBalance = calTotalIncome - calTotalExpense;
+  totalBalance.textContent = `₹${calTotalBalance.toLocaleString("en-IN")}`;
+  if (calTotalBalance >= 0) {
+    totalBalance.classList.add('text-green-600')
+  } else {
+    totalBalance.classList.add('text-red-600')
+  }
 }
