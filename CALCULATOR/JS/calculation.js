a2ca8f2ea73f99f4e5f765451fff05573742e3fa
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-07-19 00:47:48 +0530

    Taking Input

--- CALCULATOR/JS/calculation.js
+++ CALCULATOR/JS/calculation.js
@@ -1,14 +1,43 @@
 console.log("Welcome User Calculator Started");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const inputNumbers = document.getElementById("numbers");
+  const display = document.getElementById("numbers");
   const result = document.getElementById("result");
   const buttons = document.querySelectorAll("#inputBody buttons");
 
-  const allLabeledElements = document.querySelectorAll("[aria-label]");
-
-    // clearing all input of resultBody
-    
+  // clearing all input of resultBody
+  display.textContent = 0;
+  result.textContent = "";
 
   let currentNumbers = "";
+
+  buttons.forEach((button) => {
+    const value = button.textContent.trim();
+    const ariaLabel = button.getAttribute("aria-label");
+    console.log(value);
+    console.log(ariaLabel);
+
+    if (value === "C") {
+      // Clear Everything
+      currentInput = "";
+      numbersDisplay.textContent = "0";
+      resultDisplay.textContent = "";
+    } else if (ariaLabel === "Backspace") {
+      // Remove last character
+      currentInput = currentInput.slice(0, -1);
+      numbersDisplay.textContent = currentInput || "0";
+      calculateResult();
+    } else if ("Equal To" === ariaLabel) {
+      calculateResult();
+    }
+
+    if (ariaLabel === "Divide") appendValue = "/";
+    if (ariaLabel === "Multiply") appendValue = "*";
+    if (ariaLabel === "Subtract") appendValue = "-";
+    if (ariaLabel === "Add") appendValue = "+";
+    if (ariaLabel === "Percentage") appendValue = "%";
+
+    if ("1" === value) {
+    }
+  });
 });
