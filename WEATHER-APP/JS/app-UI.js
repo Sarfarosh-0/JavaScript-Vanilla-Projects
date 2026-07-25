9385b9f37dd355d30c80e46c2aaa1af9cd1b89a7
Sarfarosh-0 <sarfaroshalam0@gmail.com>
2026-07-26 00:33:06 +0530

    Main Location Card

--- WEATHER-APP/JS/app-UI.js
+++ WEATHER-APP/JS/app-UI.js
@@ -20,3 +20,36 @@ function closeSidebar() {
   sidebar.classList.add("-translate-x-full");
   mainContent.classList.remove("ml-64");
 }
+
+// Date and Time
+console.log("Date and Time");
+
+function UpdateDate() {
+  const date = new Date();
+  document.getElementById("Month").textContent = date.toLocaleDateString(
+    "en-us",
+    { month: "long" },
+  );
+  document.getElementById("Date").textContent = date.getDate() + ",";
+  document.getElementById("Year").textContent = date.getFullYear();
+  document.getElementById("WeekDay").textContent = date.toLocaleDateString(
+    "en-us",
+    { weekday: "long" },
+  );
+}
+
+UpdateDate();
+setInterval(UpdateDate, 60000);
+
+function UpdateClock() {
+  const date = new Date();
+  document.getElementById("Hour").textContent = String(
+    date.getHours(),
+  ).padStart(2, "0");
+  document.getElementById("Minutes").textContent = String(
+    date.getMinutes(),
+  ).padStart(2, "0");
+}
+
+UpdateClock();
+setInterval(UpdateClock,1000)
